a1b3c987be149e812305b172d03eb67e1612be53
gsmanivel@gmail.com <gsmanivel@gmail.com>
2023-02-02 18:19:23 -0500

    section-1

---
+++ spring-security-s1-basic/src/main/java/com/manman/springsecuritys1basic/SpringSecurityS1BasicApplication.java
@@ -0,0 +1,13 @@
+package com.manman.springsecuritys1basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityS1BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityS1BasicApplication.class, args);
+	}
+
+}
---
+++ spring-security-s1-basic/src/test/java/com/manman/springsecuritys1basic/SpringSecurityS1BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manman.springsecuritys1basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityS1BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
